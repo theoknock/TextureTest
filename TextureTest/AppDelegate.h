1b627ab55f0891dc99015c05ade6fe21e1c4e18b
James Alan Bush <jamesalanbush1972@gmail.com>
2022-01-15 05:46:54 -0500

    Initial Commit

---
+++ TextureTest/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  TextureTest
+//
+//  Created by Xcode Developer on 1/15/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextureTest/GameViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameViewController.h
+//  TextureTest
+//
+//  Created by Xcode Developer on 1/15/22.
+//
+
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "Renderer.h"
+
+// Our iOS view controller
+@interface GameViewController : UIViewController
+
+@end
